d898cd3298431c8049d12d00427ea73cc463c9e4
Jos <joheandroid@gmail.com>
2021-12-14 18:48:08 -0300

    Fix

--- soloxhot/index.py
+++ soloxhot/index.py
@@ -1,12 +1,17 @@
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 import qrcode
+
 from openpyxl import Workbook
 import xlrd
 import time
 import locale
 from currencies import Currency
 import locale
+from rich import print
+
+import soloxhoy
+
 
 locale.setlocale(locale.LC_MONETARY, 'en_US.UTF-8')
 
@@ -22,6 +27,22 @@ def format_float(num):
 
 def remove_trailing_zeros(x):
     return str(x).rstrip('5').rstrip('0').rstrip('.')
+
+def draw_multiple_line_text(image, text, font, text_color, text_start_height):
+    '''
+    From unutbu on [python PIL draw multiline text on image](https://stackoverflow.com/a/7698300/395857)
+    '''
+    draw = ImageDraw.Draw(image)
+    image_width, image_height = image.size
+    y_text = text_start_height
+    lines = textwrap.wrap(text, width=28)
+    for line in lines:
+        line_width, line_height = font.getsize(line)
+        ##draw.text(((image_width - line_width) / 2, y_text), 
+        #          line, font=font, fill=text_color)
+        draw.text(((image_width - line_width) / 2, y_text), 
+                  line, font=font, fill=text_color)
+        y_text += line_height
 
 workbook = xlrd.open_workbook("P_P.xlsx","rb")
 sheets = workbook.sheet_names()
@@ -66,182 +87,7 @@ loopPlatformProducto = [(500, 500), (500, 500), (600, 600), (250, 250), (400, 40
 
 
 
-#
-
-for j in range(len(total_producto)):
-   print('valor padre: ' + str(j))
-   for i in range(len(platform)):
-      print('valor: ' + str(i))
-      diffTop = 0
-      if i == 2:
-         diffTop = 70
-      if i == 3:
-         diffTop = 220
-
-         
 
 
-      if i != 10:
-         print('Usando plataforma: ' + str(bgPlatform[i]))
-         print('Tamaño general: ' + str(loopPlatform[i]))
-         
-         producto_img = Image.open('image/' + str(j) + '.png').convert('RGBA')
-        
-      
-         if i == 3:
-            producto_img = producto_img.resize(loopPlatformProducto[i], Image.ANTIALIAS)
-         else:
-            producto_img = producto_img.resize(loopPlatformProducto[i])
+soloxhoy.igPost(total_producto, bgPlatform, loopPlatform, loopPlatformProducto)
 
-         #calugin_img = Image.open('soloxhoy-02.png')
-         #calugin_sizeW, calugin_imgH = loopPlatform[i]
-         #calugin_img = calugin_img.resize((round(calugin_sizeW / 2 - 150), 120))
-
-
-         armado = Image.open(bgPlatform[i]).convert('RGBA')
-         
-
-         
-         
-         background = Image.new('RGB', (loopPlatform[i]), (255, 255, 255))
-         img_w, img_h = producto_img.size
-         bg_w, bg_h = armado.size
-         if i == 3:
-            #diffC = (bg_w - img_w) // 2
-            #diffC = diffC + diffC  - diffC  // 2
-            #diffC = (bg_w - img_w) // 2 + 200
-            ##diffC = (bg_w - diffC // 2) - 100
-            center =  bg_w // 3
-            center = center // 2
-            center = bg_w - center - img_w
-            diffC = center
-            print(center)
-         
-            offset = (center, 0)
-         elif i == 4:
-            center = (bg_w // 2)
-            center = center // 2
-
-            vertical = (bg_h // 2)
-            vertical = vertical // 2
-            print(vertical)
-
-
-            offset = ( center + (bg_w - img_w) // 2, 0 - vertical // 2 + 50)
-         else:
-            offset = ((bg_w - img_w) // 2, (bg_h - img_h - 50) // 2 + diffTop)
-
-
-         canvas = ImageDraw.Draw(armado)   
-         if i == 3:             
-            font = ImageFont.truetype('Pangram-ExtraBold.otf', size=28)
-         elif i == 4:
-            font = ImageFont.truetype('Pangram-ExtraBold.otf', size=22)
-         else:
-            font = ImageFont.truetype('Pangram-ExtraBold.otf', size=48)
-         
-         if i == 3:             
-            font2 = ImageFont.truetype('Pangram-Light.otf', size=20)
-         else:
-            font2 = ImageFont.truetype('Pangram-Light.otf', size=40)
-
-
-      
-         font3 = ImageFont.truetype('Pangram-ExtraBold.otf', size=70)
-         text_width, text_height = canvas.textsize("17-25-2021", font=font)
-
-
-         titulo = productos_titulo[j].upper()
-         marca = marca_titulo[j].upper()
-         
-         precio = locale.currency(precio_titulo[j], grouping=True)
-         precio = precio.replace(',', '.')
-         # substring = precio[:-3]
-         precio = precio[0:-3]
-
-         cuadradito = Image.open('cuadradito.png').convert('RGBA')
-         #cuadradito = cuadradito.resize((round(calugin_sizeW / 2 - 150), 120))
-         tw, th = canvas.textsize(precio, font=font3)
-         tw1, th1 = canvas.textsize(titulo, font=font)
-         cuadradito = cuadradito.resize((tw + 20 , th + 20))
-
-         if i == 3:
-            
-            lines = textwrap.wrap(titulo, width=50)
-         else:
-            lines = textwrap.wrap(titulo, width=50)
-         current_h, pad = 180, 310
-
-      
-
-         if i == 3:
-            alturaTitulo = 0
-         else:
-            alturaTitulo = (bg_h / 2) + (img_h / 2)
-
-
-         margin = offset2 = 40
-         for line in lines:
-            w, h = canvas.textsize(line, font=font)
-            if i == 3:
-               canvas.text(( diffC,  offset2 + alturaTitulo - 10 + diffTop), line, font = font, fill = "#FFF", align='center')
-            elif i == 4:
-               canvas.text((center + (bg_w - img_w) // 2,  img_h ), line, font = font, fill = "#FFF", align='center')
-            else:
-               canvas.text(((bg_w - w) / 2,  alturaTitulo + diffTop), line, font = font, fill = "#FFF")
-            offset2 += font.getsize(line)[1]
-
-         lines = textwrap.wrap(marca, width=50)
-         current_h, pad = 180, 310
-
-         for line in lines:
-            w, h = canvas.textsize(marca, font=font2)
-            if i == 3:
-               canvas.text((center, alturaTitulo + 70 + diffTop), marca, font = font2, fill = "#FFF")
-            elif i == 4:
-               tw1, th1 = canvas.textsize(titulo, font=font)
-               print(tw1 // 2)
-               canvas.text((center + (bg_w - img_w) // 2 + 117,  img_h + 30 ), line, font = font2, fill = "#FFF", align='center')
-            else:
-               canvas.text(((bg_w - w) / 2, alturaTitulo + 60 + diffTop), marca, font = font2, fill = "#FFF")
-            offset2 += font.getsize(marca)[1]
-
-
-         
-            
-      
-         cuadraditoW = (bg_w - cuadradito.size[0]) // 2
-         cuadraditoH = remove_trailing_zeros(floatToString((bg_h // 2) + loopPlatformProducto[i][1] - 130 + diffTop))
-
-
-         armado.paste(armado, (0,0))
-         armado.paste(producto_img, offset, producto_img)
-         #armado.paste(calugin_img, (0, 100), calugin_img)
-
-         if i == 3:
-            armado.paste(cuadradito,  (diffC, int(alturaTitulo + 140 + diffTop)), cuadradito)
-         elif i == 4:
-            armado.paste(cuadradito,  (center + (bg_w - img_w) // 2, int(alturaTitulo - 20 + diffTop)), cuadradito)
-         else:
-            armado.paste(cuadradito,  (int(cuadraditoW), int(alturaTitulo + 140 + diffTop)), cuadradito)
-         
-         lines = textwrap.wrap(precio, width=50)
-         current_h, pad = 180, 310
-
-
-         
-      
-         for line in lines:
-            w, h = canvas.textsize(precio, font=font3)
-            if i == 3:
-               canvas.text((diffC, alturaTitulo + 140 + diffTop), precio, font = font3, fill = "#FFF")
-            elif i == 4:
-               canvas.text((center + (bg_w - img_w) // 2, alturaTitulo - 16), precio, font = font3, fill = "#FFF")
-            else:
-               canvas.text(((bg_w - w) / 2, alturaTitulo + 140 + diffTop), precio, font = font3, fill = "#FFF")
-            offset2 += font.getsize(precio)[1]
-         armado.show()
-         #(bg_h // 2) + loopPlatformProducto[i][1] - 135 + cuadradito.size[1] / 2 - h / 2
-         armado.save('save/' + platform[i] + '_' + str(j) + '_.png')
-      else:
-         print(i)
---
+++ soloxhot/soloxhoy.py
@@ -0,0 +1,59 @@
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+import qrcode
+from openpyxl import Workbook
+import xlrd
+import master as x
+
+
+def igPost(total_producto, bgPlatform, loopPlatform, loopPlatformProducto):
+   mainPlatform = 'img/armado/post-ig.png'
+   for j in range(len(total_producto)):
+     
+      producto_img = Image.open('image/' + str(j) + '.png').convert('RGBA')
+      producto_img = producto_img.resize(loopPlatformProducto[0])
+
+      armado = Image.open(mainPlatform).convert('RGBA')
+      background = Image.new('RGB', (loopPlatform[0]), (255, 255, 255))
+      img_w, img_h = producto_img.size
+      bg_w, bg_h = armado.size
+
+      offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+      offsetVertical = ((bg_h - img_h) // 2)
+      offsetHorizontal = ((bg_w - img_w) // 2)
+
+      canvas = ImageDraw.Draw(armado)   
+
+      font = ImageFont.truetype('Pangram-Bold.otf', 48)
+      font2 = ImageFont.truetype('Pangram-Light.otf', 38)
+      font3 = ImageFont.truetype('Pangram-Light.otf', 38)
+      titulo = "Esta es la descripci" 
+      marca = "Esto es una marca"
+      precio = "$ 90.000"
+      
+
+      cuadradito = Image.open('cuadradito.png').convert('RGBA')
+      tw, th = canvas.textsize(precio, font=font3)
+      cuadradito = cuadradito.resize((tw + 20 , th + 20))
+      cuadraditoW = (bg_w - cuadradito.size[0]) // 2
+
+      diffTitulo = textwrap.wrap(titulo, width=28)
+      
+      if len(diffTitulo) == 1:
+         diff = 65
+         diffCuadrito = 75
+      else:
+         diff = 135
+         diffCuadrito = 115
+
+      x.draw_multiple_line_text(armado, titulo, font, '#FFF', offsetVertical + img_h)
+      x.draw_multiple_line_text(armado, marca, font2, '#FFF', offsetVertical + img_h + diff)
+
+      armado.paste(armado, (0,0))
+      armado.paste(producto_img, offset, producto_img)
+      armado.paste(cuadradito, (int(cuadraditoW), offsetVertical + img_h + diff + diffCuadrito), cuadradito)
+
+      x.draw_multiple_line_text(armado, precio, font3, '#FFF', offsetVertical + img_h + diff + diffCuadrito + 5)
+      armado.show()
+     
+        
