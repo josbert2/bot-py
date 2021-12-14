15066aaa6a6a977ef8ccde6e7a36bd1abfb2849e
Jos <joheandroid@gmail.com>
2021-12-14 11:50:29 -0300

    Fix

--- mail/index.py
+++ mail/index.py
@@ -14,11 +14,22 @@ import ssl
 import certifi
 from urllib import request as rq
 from io import BytesIO
+from openpyxl import Workbook
+import xlrd
+from selenium.common.exceptions import NoSuchElementException
 
-import time
 
 
-from rich import print
+workbook = xlrd.open_workbook("link.xlsx","rb")
+sheets = workbook.sheet_names()
+productos_link = []
+
+for sheet_name in sheets:
+    sh = workbook.sheet_by_name(sheet_name)
+    for rownum in range(sh.nrows):
+        row_valaues = sh.row_values(rownum)
+        productos_link.append(row_valaues[0])
+     
 
 
 def draw_multiple_line_text(image, text, font, text_color, text_start_height):
@@ -37,52 +48,90 @@ def draw_multiple_line_text(image, text, font, text_color, text_start_height):
                   line, font=font, fill=text_color)
         y_text += line_height
 
-url = 'https://www.entrekids.cl/producto/organo-electrico-nup03-61-teclas-color-negro'
 
-"""
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get(url)
+for i in range(len(productos_link)):
+    print("Scan:" + str(i))
 
-nombre = driver.find_element_by_css_selector('.h1-alert')
-imagen = driver.find_element_by_css_selector('.zoomImg')
+    
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get(productos_link[i])
 
+    nombre = driver.find_element_by_css_selector('.h1-alert').text
+    imagen = driver.find_element_by_css_selector('.zoomImg')
+    imagen = imagen.get_attribute('src')
 
-precioNormal = []
-precioOferta = []
-
-if len(driver.find_element_by_css_selector('#price-none')[0]) > 0:
-    precioNormal.append(driver.find_element_by_css_selector('#price-none')[0].text) 
-if len(driver.find_element_by_css_selector('.precio-oferta-div')[0]) > 0:
-    precioOferta.append(driver.find_element_by_css_selector('.precio-oferta-div')[0].text)
-"""
-background = Image.new('RGB', ((300, 465)), (255, 255, 255))
-backgroundButton = Image.open('btn.png')
-
-
-font = ImageFont.truetype('Pangram-Bold.otf', 17)
-font2 = ImageFont.truetype('Pangram-Light.otf', 14)
-userinput = "Lorem ipsum dolor sit amet asdad"
-
-
-producto = Image.open('unnamed.jpg').convert('RGBA')
-producto = producto.resize((276, 276))
-
-print(background.size[1])
-
-
-background.paste(producto, (0, 0), producto)
-background.paste(backgroundButton, (0, background.size[1] - backgroundButton.size[1]), backgroundButton)
-
-print(len(textwrap.wrap(userinput, width=28)))
+    response = requests.get(imagen)
+    Image.open(BytesIO(response.content)).convert('RGBA').save('static/image-' + str(i) + '.png')
 
 
 
-draw_multiple_line_text(background, userinput, font, '#41494f', producto.size[1])
-draw_multiple_line_text(background, "$90.000 | 35%", font, 'rgba(245,106,128, 1)', producto.size[1] + 53)
-draw_multiple_line_text(background, "precio normal: $25.245", font2, '#41494f', producto.size[1] + 83)
+
+    precioNormal = 0
+    precioOferta = 0
+
+    try:
+        element = driver.find_element_by_class_name('price-big').find_element_by_class_name('price-none')
+        precioNormal = driver.find_element_by_css_selector('.price-big .price-none').text
+    except NoSuchElementException:
+        precioNormal = 0
+        print("No element found")
 
 
-background.show()
+    try:
+        element = driver.find_element_by_css_selector('.precio-oferta-div')
+        precioOferta = driver.find_element_by_css_selector('.precio-oferta-div').text 
+        precioOferta = precioOferta.replace("(oferta)","")
+    except NoSuchElementException:
+        precioOferta = 0
+        print("No element found")
+
+
+
+    try:
+        element = driver.find_element_by_css_selector('.porcentaje-email')
+        descuento = driver.find_element_by_css_selector('.porcentaje-email').text 
+    except NoSuchElementException:
+        descuento = 0
+        print("No element found")
+  
+    
+    background = Image.new('RGB', ((300, 465)), (255, 255, 255))
+    backgroundButton = Image.open('btn.png')
+
+
+    font = ImageFont.truetype('Pangram-Bold.otf', 17)
+    font2 = ImageFont.truetype('Pangram-Light.otf', 14)
+    userinput = nombre
+
+
+    producto = Image.open('static/image-' + str(i) + '.png').convert('RGBA')
+    producto = producto.resize((276, 276))
+
+    print(background.size[1])
+
+
+    background.paste(producto, (0, 0), producto)
+    background.paste(backgroundButton, (0, background.size[1] - backgroundButton.size[1]), backgroundButton)
+
+    diff = textwrap.wrap(userinput, width=28)
+
+
+
+    draw_multiple_line_text(background, userinput, font, '#41494f', producto.size[1])
+    #draw_multiple_line_text(background, "$90.000 | 35%", font, 'rgba(245,106,128, 1)', producto.size[1] + 53)
+
+
+    if precioOferta[i] != 0:
+        descuento = descuento.replace("DCTO", "")
+        draw_multiple_line_text(background, precioOferta + ' | ' + descuento, font, '#f06980', producto.size[1] + 53)
+    else:
+        draw_multiple_line_text(background, " ", font, '#f06980', producto.size[1] + 53)
+
+    
+    draw_multiple_line_text(background, "precio normal:" + precioNormal, font2, '#41494f', producto.size[1] + 83)
+
+
+    background.show()
 
 
 
